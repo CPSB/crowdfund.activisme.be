2ef07348892eb0a9bf168b9aa7eaff2b2ff73655
Tim Joosten <Tjoosten3@gmail.com>
2017-07-19 19:55:59 +0200

    revert moree

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,12 +3,6 @@
 @section('title', 'Index')
 
 @section('content')
-    <div class="row">
-        <div class="col-sm-12">
-            <img class="img-rounded img-front" src="{{ asset('img/front.jpg') }}">
-        </div>
-    </div>
-
     @if (session()->get('class') && session()->get('message'))
         <div style="padding-top: 15px;" class="row">
             <div class="col-md-12">
@@ -20,30 +14,10 @@
         </div>
     @endif
 
-    <div style="padding-top: 15px;" class="row">
+    <div class="row">
         <div class="col-md-12">
             <div class="panel panel-default">
                 <div class="panel-body">Application barebone.</div>
-            </div>
-        </div>
-    </div>
-@endsection
-
-@section('footer')
-    <div class="copyright">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6 col-sm-4">
-                    <p>© {{ date('Y') }} - ActivismeBE</p>
-                </div>
-
-                <div class="col-md-6 col-sm-8">
-                    <ul class="bottom_ul">
-                        <li><a href="http://www.activisme.be">ActivismeBE</a></li>
-                        <li><a href="https://www.vrede.be/">Vrede.be</a></li>
-                        <li><a href="https://stopnato2017.org/">StopNATO2017</a></li>
-                    </ul>
-                </div>
             </div>
         </div>
     </div>
