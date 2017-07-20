ee6fec8f337047ff3fcb20fb1f0181c69c324c9d
Tim Joosten <Tjoosten3@gmail.com>
2017-07-20 09:56:31 +0200

    Implement mass-assign fields

--- app/Finance.php
+++ app/Finance.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Finance extends Model
 {
-    //
+    /**
+     * Mass-assign fields for the database table. 
+     * 
+     * @var array
+     */
+    protected $fillable = ['type', 'finance_plan', 'uitvoerder', 'amount', 'creator_id', 'extra_informatie']; 
+
+    /**
+     * Creator transaction relation data. 
+     *
+     * @return void
+     */
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'creator_id');
+    }
 }
