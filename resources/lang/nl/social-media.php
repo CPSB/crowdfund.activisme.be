9ba4a9a30b286277a5632fb162b66ef712fe6260
Tim Joosten <Tjoosten3@gmail.com>
2017-07-31 04:53:40 +0200

    Implement Dutch social media SEO

---
+++ resources/lang/nl/social-media.php
@@ -0,0 +1,9 @@
+<?php
+
+// TODO: FR en EN translation.
+
+return [
+    'title'         => 'Activisme_BE Crowdfund voor de werking en Vredes caravan.',
+    'description'   => 'Met ons klein team dat opkomt voor wereldvrede en de rechten van de mens, gebruiken we deze website, activisme.be, als uitvalsbasis.
+                        Activisme.be wil een platform bieden om mensen en organisaties samen te brengen en vanuit een verenigd front te..'
+];
