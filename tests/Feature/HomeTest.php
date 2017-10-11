9632ad8b3d9f50a8bae3c18eb254295477ca2ba2
Tjoosten <Tjoosten3@gmail.com>
2017-10-11 02:03:48 +0200

    Complete disclaimer and home test

--- tests/Feature/HomeTest.php
+++ tests/Feature/HomeTest.php
@@ -2,29 +2,62 @@
 
 namespace Tests\Feature;
 
-use ActivismeBE\User;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
+/**
+ * Class HomeTest
+ *
+ * @package Tests\Feature
+ */
 class HomeTest extends TestCase
 {
     use DatabaseMigrations, DatabaseTransactions;
 
+    /**
+     * @test
+     * @covers \ActivismeBE\Http\Controllers\HomeController::index()
+     */
     public function testHomeView()
     {
         $this->get(route('index'))->assertStatus(200);
     }
 
+    /**
+     * @test
+     * @covers \ActivismeBE\Http\Controllers\HomeController::backend()
+     */
     public function testHomeBackendView()
     {
         // TODO:  Extends to some test with all the possible users.
 
-        $user = $this->createAdmin();
+        $user       = $this->createUser();
+        $blocked    = $this->createBlockedUser();
+        $admin      = $this->createAdmin();
 
+        // Test access when unauthenticated
+        $this->get(route('home'))->assertRedirect('/');
+
+        // Test access when user is a normal user.
         $this->actingAs($user)
             ->seeIsAuthenticatedAs($user)
+            ->get(route('home'))
+            ->assertStatus(403);
+
+        auth()->logout();
+
+        // test when the user is blocked.
+        $this->actingAs($blocked)
+            ->seeIsAuthenticatedAs($blocked)
+            ->get(route('home'))
+            ->assertStatus(403);
+
+        auth()->logout();
+
+        // Test access when user is admin.
+        $this->actingAs($admin)
+            ->seeIsAuthenticatedAs($admin)
             ->get(route('home'))
             ->assertStatus(200);
     }
--- tests/Feature/SocialAuthencationTest.php
+++ tests/Feature/SocialAuthencationTest.php
@@ -8,14 +8,31 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
+/**
+ * Class SocialAuthencationTest
+ *
+ * @package Tests\Feature
+ */
 class SocialAuthencationTest extends TestCase
 {
+    /**
+     * @test
+     *
+     * @covers \ActivismeBE\Http\Controllers\Auth\SocialAuthencation::redirectToProvider()
+     * @covers \ActivismeBE\Http\Controllers\Auth\SocialAuthencation::handleProviderCallback()
+     */
     public function testFacebookAuthencation()
     {
         Socialite::shouldReceive('driver->fields->scopes->user')->with('facebook')->andReturn(true);
         $this->get(route('social', ['callback' => 'facebook']));
     }
 
+    /**
+     * @test
+     *
+     * @covers \ActivismeBE\Http\Controllers\Auth\SocialAuthencation::redirectToProvider()
+     * @covers \ActivismeBE\Http\Controllers\Auth\SocialAuthencation::handleProviderCallback()
+     */
     public function testTwitterAuthencation()
     {
         Socialite::shouldReceive('driver->fields->scopes->user')->with('twitter')->andReturn(true);
